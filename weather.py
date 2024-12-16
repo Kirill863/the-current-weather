54b93cf6fa75a90bb0d08e583e64698aaf73a580
Kirill863 <Byblik4310@gmail.com>
2024-12-16 21:28:39 +0300

    добавил переменные

--- weather.py
+++ weather.py
@@ -12,12 +12,17 @@ pip install plyer requests pyinstaller
 
 """
 
-from urllib import response
 import requests
 from plyer import notification
 # Просто сделаем запрос без функций
 
-url = r'https://api.openweathermap.org/data/2.5/weather?q=Москва&appid=23496c2a58b99648af590ee8a29c5348&units=metric&lang=ru'
+CITY = "Щёлково"
+API_KEY = "23496c2a58b99648af590ee8a29c5348"
+UNITS = "metric"
+LANG = "ru"
+
+
+url= fr'https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units={UNITS}&lang={LANG}'
 
 response = requests.get(url) # Сделали запрос и получили объект ответа
 print(response.status_code) # Получили статус ответа
@@ -38,7 +43,10 @@ print(f'Температура: {temp}°C\nОщущается как: {feels_lik
 
 # Уведомление
 notification.notify(
-title='Погода в Москве',
-message=f'Температура: {temp}°C\nОщущается как: {feels_like}°C\nОписание: {description}',
+title= 'Погода в Щёлково',
+message= f'Температура: {temp}°C\nОщущается как: {feels_like}°C\nОписание: {description}',
 app_name='Weather',
-app_icon=None)
+app_icon = None,
+timeout = 10,
+toast = False,
+)
