6a63c481e9af321cfa8d677321857906e91de3dc
Kirill863 <Byblik4310@gmail.com>
2024-12-17 19:22:56 +0300

    переустановил среду, создал новый exe

--- weather.py
+++ weather.py
@@ -1,5 +1,5 @@
-import requests  # type: ignore
-from plyer import notification  # type: ignore
+import requests
+from plyer import notification
 
 CITY = "Щёлково"
 API_KEY = "23496c2a58b99648af590ee8a29c5348"
